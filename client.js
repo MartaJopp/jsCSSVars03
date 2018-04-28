e8f9ba0ca26f3c3f81eea690b322629442bf5622
Marta Jopp <martapeterson@gmail.com>
2018-04-27 20:36:29 -0500

    sourced js in after html

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
-console.log('sourced')
+console.log('sourced');
+
+const inputs = document.querySelectorAll('.controls input');
+//querySelector returns a NodeList which looks like an array but 
+//does not have all the methods it has. It has keys and entries.
+
+function handleUpdate() {
+    console.log(this.value)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
